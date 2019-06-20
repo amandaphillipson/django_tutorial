4628a182c6f3afcaf53270a7d98088caa629aa2a
Amanda Phillipson <amanda.minton@gmail.com>
2019-06-20 09:35:24 -0400

    Django superuser set up, library started.

--- learn_django/urls.py
+++ learn_django/urls.py
@@ -35,16 +35,12 @@ urlpatterns += [
     path('', RedirectView.as_view(url='/catalog/', permanent=True)),
 ]
 
+
 # Use static() to add url mapping to serve static files during development (only)
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-from django.urls import path
-from . import views
 
-urlpatterns = [
-
-]
 
